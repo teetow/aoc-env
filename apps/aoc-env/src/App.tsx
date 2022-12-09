fb62c4f427d46fcb5d4600c5e19f197a6dfa1121
Johan Althoff <teetow@gmail.com>
2022-12-09 19:13:07 +0100

    intermezzo: start at current day by default

--- apps/aoc-env/src/App.tsx
+++ apps/aoc-env/src/App.tsx
@@ -1,15 +1,38 @@
 import { HashRouter, Route, Routes, useParams } from "react-router-dom";
 import "./App.scss";
+import years from "./data";
+import { pad2 } from "./lib/utils";
 import Blurb from "./ui/Blurb";
 import Debugger from "./ui/Debugger";
 import Picker from "./ui/Picker";
 import Runner from "./ui/Runner";
 
+const getDate = (year: number, day: number) => {
+  let validYear = 2021;
+  let validDay = 1;
+
+  if (Number.isNaN(year)) year = new Date().getFullYear();
+
+  if (Number.isNaN(day)) {
+    day = new Date().getDate();
+  }
+
+  if (Object.keys(years).includes(year.toString())) {
+    validYear = year;
+
+    if (Object.keys(years[year]).includes(`day${pad2(day)}`)) {
+      validDay = day;
+    } else {
+      validDay = Object.keys(years[year]).length; // NOT off by one, days are 1-indexed
+    }
+  }
+  return [validYear, validDay];
+};
+
 const AppMain = () => {
   let params = useParams();
-  const year = Number(params.year) || 2021;
-  const day = Number(params.day) || 1;
-
+  const [year, day] = getDate(Number(params.year), Number(params.day));
+  console.log(day);
   return (
     <>
       <Picker year={year} day={day} />
